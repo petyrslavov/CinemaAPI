8e4ee5db84d3e2d114f43efca6c36d30092087fb
petyrslavov <petyr_slavov@abv.bg>
2019-09-14 13:24:19 +0300

    Implemented first task

---
+++ CinemAPI/Models/Input/Projection/ProjectionCreationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemAPI.Models.Input.Projection
+{
+    public class ProjectionCreationModel
+    {
+        public int RoomId { get; set; }
+
+        public int MovieId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int AvailableSeatsCount { get; set; }
+    }
+}
